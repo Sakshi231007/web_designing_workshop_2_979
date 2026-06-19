c8b369377b0102ae07492fea9bdc5d06aac34f5e
Sakshi231007 <singhalsakshi486@gmail.com>
2026-06-19 12:51:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-function findFactorial() {
-    let n = parseInt(document.getElementById("num").value);
-    let result = document.getElementById("result");
+function calculateFactorial() {
+    let num = parseInt(document.getElementById("num").value);
 
-    if (isNaN(n)) {
-        result.innerHTML = "Please enter a valid number.";
+    if (isNaN(num) || num < 0) {
+        document.getElementById("result").innerHTML =
+            "Please enter a non-negative number.";
         return;
     }
 
-    if (n < 0) {
-        result.innerHTML = "Factorial is not defined for negative numbers.";
-        return;
+    let factorial = 1;
+
+    for (let i = 1; i <= num; i++) {
+        factorial *= i;
     }
 
-    let fact = 1;
-
-    for (let i = 1; i <= n; i++) {
-        fact *= i;
-    }
-
-    result.innerHTML = "Factorial of " + n + " is " + fact;
+    document.getElementById("result").innerHTML =
+        "Factorial of " + num + " is " + factorial;
 }
